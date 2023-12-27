390e0daa210f866236532646e96fca7c748856b6
Osie68 <sielamoussaouattara@gmail.com>
2023-12-27 11:25:15 +0000

    calculer montant

--- src/app/views/mvt-stock/entree/add-edit-ligne-entree/add-edit-ligne-entree.component.ts
+++ src/app/views/mvt-stock/entree/add-edit-ligne-entree/add-edit-ligne-entree.component.ts
@@ -1,17 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { ILigneEntree, LigneEntree } from 'src/app/entities/ligne-entree.model';
+import { MedailleService } from '../../../../services/medaille.service';
+import { IMedaille } from 'src/app/entities/medaille.model';
 
 @Component({
   selector: 'app-add-edit-ligne-entree',
   templateUrl: './add-edit-ligne-entree.component.html',
   styleUrl: './add-edit-ligne-entree.component.scss'
 })
-export class AddEditLigneEntreeComponent {
+export class AddEditLigneEntreeComponent implements OnInit {
 
-  constructor(private activeModal: NgbActiveModal) {}
-  
   ligneEntree: ILigneEntree = new LigneEntree();
+  medailles?: IMedaille[];
+
+  constructor(
+    private activeModal: NgbActiveModal,
+    private medailleService: MedailleService
+  ) {}
+
+  ngOnInit(): void {
+      
+  }
 
   save(): void {
     this.activeModal.close(this.ligneEntree);
@@ -19,6 +29,12 @@ export class AddEditLigneEntreeComponent {
 
   close(): void {
     this.activeModal.dismiss();
+  }
+
+  calculerMontant($event: any): void {
+    console.log('calcul...', $event)
+    if(undefined != this.ligneEntree.prixUnitaire  && undefined != this.ligneEntree.quantiteLigne)
+      this.ligneEntree.montantLigne = this.ligneEntree.prixUnitaire * this.ligneEntree.quantiteLigne;
   }
 
 }
